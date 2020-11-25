5eaea086b6f8149e1a98e5624c9281301feb34f1
tiagomartins91 <tiago_alex_martins@hotmail.com>
2020-11-25 23:39:38 +0000

    Add more additional fields to courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,10 @@ User.create_with(email: 'admin@example', password: 'admin@example', password_con
   Course.create!([{
     title: Faker::Educator.course_name,
     description: Faker::TvShows::GameOfThrones.quote,
-    user_id: User.first.id
+    user_id: User.first.id,
+    short_description: Faker::Quote.famous_last_words,
+    language: Faker::ProgrammingLanguage.name,
+    level: 'Beginner',
+    price: Faker::Number.between(from: 1000, to: 2000)
   }])
 end
