4e9dd64887b865fdd8e3f0cd4ea7840f9a822dc5
tiagomartins91 <tiago_alex_martins@hotmail.com>
2020-11-28 18:37:49 +0000

    Add search courses to header

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,13 +4,15 @@ class CoursesController < ApplicationController
   # GET /courses
   # GET /courses.json
   def index
-    if params[:title]
-      @courses = Course.where("title ILIKE ?", "%#{params[:title]}%")
-    else
+    #if params[:title]
+    #  @courses = Course.where("title ILIKE ?", "%#{params[:title]}%")
+    #else
       #@courses = Course.all
-      @q = Course.ransack(params[:q])
-      @courses = @q.result.includes(:user)
-    end
+      #@q = Course.ransack(params[:q])
+      #@courses = @q.result.includes(:user)
+    #end
+    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_path)
+    @courses = @ransack_courses.result.includes(:user)
   end
 
   # GET /courses/1
