1d1509e98e5fdcae8a7ab3fe8eaa0a81dab43d4e
tiagomartins91 <tiago_alex_martins@hotmail.com>
2020-12-05 13:41:01 +0000

    Add Lessons table and styling views

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,7 @@ class Course < ApplicationRecord
   validates :description, presence: true, length: { :minimum => 5 }
 
   belongs_to :user
+  has_many :lessons, dependent: :destroy
 
   include PublicActivity::Model
   tracked owner: Proc.new{ |controller, model| controller.current_user rescue nil }
