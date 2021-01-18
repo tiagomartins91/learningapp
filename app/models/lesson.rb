61805cfbf1ad5891b94859d42c888d09fd7461d9
tiagomartins91 <tiago_alex_martins@hotmail.com>
2021-01-18 20:27:55 +0000

    Add validation to lesson title

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,6 +5,7 @@ class Lesson < ApplicationRecord
   has_many :user_lessons, dependent: :destroy
 
   validates :title, :content, :course, presence: true
+  validates :title, length: { :maximum => 70 }
 
   has_rich_text :content
 
