7acf3a7d175b0dd69ba5774d0668941ee32fc292
tiagomartins91 <tiago_alex_martins@hotmail.com>
2021-01-22 16:09:54 +0000

    Add video preview to lesson action text

--- app/javascript/trix-editor-overrides.js
+++ app/javascript/trix-editor-overrides.js
@@ -1,11 +1,12 @@
 // Blacklist all file attachments
+/*
 window.addEventListener("trix-file-accept", function (event) {
     event.preventDefault()
     alert("File attachment not supported!")
 })
 
 // Only images
-/*
+
 window.addEventListener("trix-file-accept", function (event) {
     const acceptedTypes = ['image/jpeg', 'image/png']
     if(!acceptedTypes.includes(event.file.type)){
